2d8f8d5a12a16375bc944bfd63a0133fd2a1a5df
Gulfam Ansari <gulfamansari1515@gmail.com>
2019-07-05 16:48:12 +0530

    build added

---
+++ build/precache-manifest.01144428b8180f25b84f70a7d1681fe2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "633100139e5fa7be279b2f35dfa85a15",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1ad0e60ae840acc44ea",
+    "url": "/static/css/main.066d65be.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "a1ad0e60ae840acc44ea",
+    "url": "/static/js/main.577a527d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
