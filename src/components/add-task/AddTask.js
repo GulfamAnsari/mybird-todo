77b0e8bfb4fd0e4a5afd6818e77ec660ad09bd71
Gulfam Ansari <gulfamansari1515@gmail.com>
2019-07-11 16:15:53 +0530

    update form handling technique

--- src/components/add-task/AddTask.js
+++ src/components/add-task/AddTask.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import './AddTask.css';
 
-export default function AddTask({ openNewTodo, toggleTodoHandler, addNewTodoHandler}) {
+export default function AddTask({ openNewTodo, toggleTodoHandler, addNewTodoHandler }) {
+
+    let formData = {
+        title: '',
+        description: '',
+        priority: 'High'
+    };
 
     function onSubmitHandler(event) {
         event.preventDefault();
-        var formElement = document.getElementById("todoForm").elements;
-        const data = {
-            title: formElement[0].value,
-            description: formElement[1].value,
-            priority: formElement[2].value 
-        }
-        addNewTodoHandler(data);
+        addNewTodoHandler(formData);
+    }
+
+    function onChangeHandler(event) {
+        formData[event.target.name] = event.target.value;
     }
 
     return (
-        <main role="main" className={ openNewTodo ? 'main-wrap' : 'hidden'}>
+        <main role="main" className={openNewTodo ? 'main-wrap' : 'hidden'}>
             <header role="heading" className="header">
                 <h1 className="app-header"> Add your task <span className="close-add-task" onClick={() => { toggleTodoHandler(false) }}>X</span></h1>
             </header>
@@ -24,22 +28,22 @@ export default function AddTask({ openNewTodo, toggleTodoHandler, addNewTodoHand
                     <form role="form" id="todoForm" onSubmit={onSubmitHandler}>
                         <div className="row">
                             <label htmlFor="title"> Title </label>
-                            <input className="form-input" name="title" id="title" type="text" placeholder="Learn JavaScript" />
+                            <input onChange={onChangeHandler} className="form-input" name="title" id="title" type="text" placeholder="Learn JavaScript" />
                         </div>
                         <div className="row">
                             <label htmlFor="description"> Description </label>
-                            <textarea id="description" name="description" rows="4" cols="30" placeholder="Coz you already know Java"></textarea>
+                            <textarea onChange={onChangeHandler} id="description" name="description" rows="4" cols="30" placeholder="Coz you already know Java"></textarea>
                         </div>
                         <div className="row">
                             <label htmlFor="priority"> Priority </label>
-                            <select id="priority" name="priority">
+                            <select onChange={onChangeHandler} id="priority" name="priority">
                                 <option value="high">High</option>
                                 <option value="medium">Medium</option>
                                 <option value="low">Low</option>
                             </select>
                         </div>
                         <div className="row submit-btn">
-                            <input type="submit" className="primary-button" value="DO IT" />
+                            <input onChange={onChangeHandler} type="submit" className="primary-button" value="DO IT" />
                         </div>
                     </form>
                 </section>
