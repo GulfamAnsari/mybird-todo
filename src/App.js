9c2a6991c6f6ef4cc1e902bcb9b65377b5a53a39
Gulfam Ansari <gulfamansari1515@gmail.com>
2019-07-11 13:32:40 +0530

    infinit scroll

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
 import './App.css';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
 import Section from './components/section/Section';
+import Login from './components/log-in/Login';
+import Scrol from './components/scrol/Scrol';
 
 export default class App extends Component {
 
   constructor(props) {
     super(props);
+    const abc = [{ name: 'abc' }, { name: 'rtyu' }, { name: 'sdfgh' }];
   }
 
   render() {
     return (
-      <React.Fragment>
-        <Header />
-        <Section />
-        <Footer />
-      </React.Fragment>
+      <Scrol />
+      // <BrowserRouter>
+      //   <React.Fragment>
+      //     <Header />
+      //     <Route path="/" exact component={Login} />
+      //     <Route path="/todos" exact component={Section} />
+      //     <Footer />
+      //   </React.Fragment>
+      // </BrowserRouter>
     )
   }
 
