40b0f47ca11a801818250c1c07bf8f103b908d92
Gulfam Ansari <gulfamansari1515@gmail.com>
2019-07-11 17:13:04 +0530

    create state for login component

--- src/components/log-in/Login.js
+++ src/components/log-in/Login.js
@@ -3,29 +3,86 @@ import './Login.css';
 import { Link } from 'react-router-dom';
 
 export default class Login extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedForm: 'sign-in',
+            signIn: {
+                username: '',
+                password: '',
+                remebmer: false
+            },
+            signUp: {
+                username: '',
+                password: '',
+                repeat: '',
+                email: ''
+            }
+        }
+    }
+
+    formSelectionHanndler(event) {
+        this.setState({
+            selectedForm: event.target.name
+        })
+    }
+
+    signInOnChangeHandler(event) {
+        const { signIn } = this.state;
+        if (event.target.name === 'remebmer') {
+            signIn[event.target.name] = event.target.checked;
+        } else {
+            signIn[event.target.name] = event.target.value;
+        }
+        this.setState({
+            signIn
+        });
+    }
+
+    signUpOnChangeHandler(event) {
+        const { signUp } = this.state;
+        signUp[event.target.name] = event.target.value;
+        this.setState({
+            signUp
+        });
+
+    }
+
+    onSubmitHandler(event) {
+        event.preventDefault();
+        console.log(this.state);
+        if (event.target.name === 'sign-in') {
+            console.log(this.state.signIn)
+        } else if (event.target.name === 'sign-up') {
+            console.log(this.state.signUp)
+        }
+    }
+
     render() {
+        const { selectedForm } = this.state;
         return (
             <div className="login">
                 <div className="login-wrap">
                     <div className="login-html">
-                        <input id="tab-1" type="radio" name="tab" className="sign-in" checked onChange={() => { }} /><label htmlFor="tab-1" className="tab">Sign In</label>
-                        <input id="tab-2" type="radio" name="tab" className="sign-up" /><label htmlFor="tab-2" className="tab">Sign Up</label>
+                        <input id="tab-1" type="radio" name="tab" className="sign-in" name="sign-in" checked={selectedForm === 'sign-in' ? true : false} onChange={this.formSelectionHanndler.bind(this)} /><label htmlFor="tab-1" className="tab">Sign In</label>
+                        <input id="tab-2" type="radio" name="tab" className="sign-up" name="sign-up" checked={selectedForm === 'sign-up' ? true : false} onChange={this.formSelectionHanndler.bind(this)} /><label htmlFor="tab-2" className="tab">Sign Up</label>
                         <div className="login-form">
-                            <div className="sign-in-htm">
+                            <form onSubmit={this.onSubmitHandler.bind(this)} className="sign-in-htm">
                                 <div className="group">
                                     <label htmlFor="user" className="label">Username</label>
-                                    <input id="user" type="text" className="input" />
+                                    <input required onChange={this.signInOnChangeHandler.bind(this)} id="user" name="username" type="text" className="input" />
                                 </div>
                                 <div className="group">
                                     <label htmlFor="pass" className="label">Password</label>
-                                    <input id="pass" type="password" className="input" data-type="password" />
+                                    <input required onChange={this.signInOnChangeHandler.bind(this)} id="pass" type="password" name="password" className="input" data-type="password" />
                                 </div>
                                 <div className="group">
-                                    <input id="check" type="checkbox" className="check" />
+                                    <input onChange={this.signInOnChangeHandler.bind(this)} id="check" type="checkbox" name="remebmer" className="check" />
                                     <label htmlFor="check"><span className="icon"></span> Keep me Signed in</label>
                                 </div>
                                 <div className="group">
-                                    <input type="submit" className="button" value="Sign In" />
+                                    <input type="submit" className="button" name="sign-in" value="Sign In" />
                                 </div>
                                 <div className="hr"></div>
                                 <div className="foot-lnk">
@@ -36,32 +93,32 @@ export default class Login extends Component {
                                     <a href="#forgot">Forgot Password?</a>
                                 </div>
 
-                            </div>
-                            <div className="sign-up-htm">
+                            </form>
+                            <form onSubmit={this.onSubmitHandler.bind(this)} className="sign-up-htm">
                                 <div className="group">
                                     <label htmlFor="user" className="label">Username</label>
-                                    <input id="user" type="text" className="input" />
+                                    <input required onChange={this.signUpOnChangeHandler.bind(this)} id="user" name="username" type="text" className="input" />
                                 </div>
                                 <div className="group">
                                     <label htmlFor="pass" className="label">Password</label>
-                                    <input id="pass" type="password" className="input" data-type="password" />
+                                    <input required onChange={this.signUpOnChangeHandler.bind(this)} id="pass" type="password" name="password" className="input" data-type="password" />
                                 </div>
                                 <div className="group">
                                     <label htmlFor="pass" className="label">Repeat Password</label>
-                                    <input id="pass" type="password" className="input" data-type="password" />
+                                    <input required onChange={this.signUpOnChangeHandler.bind(this)} id="pass" type="password" name="repeat" className="input" data-type="password" />
                                 </div>
                                 <div className="group">
                                     <label htmlFor="pass" className="label">Email Address</label>
-                                    <input id="pass" type="text" className="input" />
+                                    <input required onChange={this.signUpOnChangeHandler.bind(this)} id="pass" type="text" name="email" className="input" />
                                 </div>
                                 <div className="group">
-                                    <input type="submit" className="button" value="Sign Up" />
+                                    <input type="submit" className="button" name="sign-up" value="Sign Up" />
                                 </div>
                                 <div className="hr"></div>
                                 <div className="foot-lnk">
                                     <label htmlFor="tab-1">Already Member?</label>
                                 </div>
-                            </div>
+                            </form>
                         </div>
                     </div>
                 </div>
