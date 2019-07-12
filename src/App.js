850797ccc1d879b439456e8caba4f764fc051e84
Gulfam Ansari <gulfamansari1515@gmail.com>
2019-07-12 19:15:54 +0530

    make seperate page for todo

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
-import Section from './components/section/Section';
 import Scrol from './components/scrol/Scrol';
 import Home from './pages/home/Home';
+import Todo from './pages/todo/Todo';
 
 export default class App extends Component {
 
@@ -20,7 +20,7 @@ export default class App extends Component {
         <React.Fragment>
           <Header />
           <Route path="/" exact component={Home} />
-          <Route path="/todos" exact component={Section} />
+          <Route path="/todos" exact component={Todo} />
           <Route path="/scroll" exact component={Scrol} />
           <Footer />
         </React.Fragment>
