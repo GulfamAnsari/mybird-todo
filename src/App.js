57d915d6ba7dedd1c4176d1d1598193a51aa6620
gulfamansari2019 <gulfamansari1515@gmail.com>
2019-07-24 15:54:48 +0530

    add text editor

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
 import Scrol from './components/scrol/Scrol';
+import TextEditor from './components/text-editor/TextEditor';
 import Home from './pages/home/Home';
 import Todo from './pages/todo/Todo';
 
@@ -23,6 +24,7 @@ export default class App extends Component {
             <Route path="/" exact component={Home} />
             <Route path="/todos" exact component={Todo} />
             <Route path="/scroll" exact component={Scrol} />
+            <Route path="/text-editor" exact component={TextEditor} />
           </Switch>
           <Footer />
         </React.Fragment>
